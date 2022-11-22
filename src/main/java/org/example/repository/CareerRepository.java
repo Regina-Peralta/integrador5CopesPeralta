8c398fee5ede1ae3df9b8c1304ea7f1a14e59219
Regina-Peralta <regiperalta01@gmail.com>
2022-11-22 14:14:08 -0300

    d2

--- src/main/java/org/example/repository/CareerRepository.java
+++ src/main/java/org/example/repository/CareerRepository.java
@@ -1,13 +1,15 @@
 package org.example.repository;
 
 import org.example.entity.Career;
+import org.example.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CareerRepository {
+public interface CareerRepository extends JpaRepository<Career,Long> {
     @Query("SELECT DISTINCT c,s.size FROM Career c JOIN c.students s WHERE s.size > 0 ORDER BY s.size DESC")
     List<Career> getAllCareersByStudentsAmount();
     @Query("SELECT DISTINCT c FROM Career c")
--- src/main/java/org/example/repository/CareerStudentRepository.java
+++ src/main/java/org/example/repository/CareerStudentRepository.java
@@ -3,20 +3,22 @@ package org.example.repository;
 
 import org.example.dto.CareerInscriptionsDTO;
 import org.example.dto.CareerReportDTO;
+import org.example.entity.CareerStudent;
 import org.example.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CareerStudentRepository {
+public interface CareerStudentRepository  extends JpaRepository<CareerStudent,Long>{
 
     @Query("SELECT DISTINCT(s) FROM Student s, CareerStudent cs WHERE cs.career.id = :idCarrera AND s.city = :ciudad")
     List<Student> getStudentsByCareerFilterCity(Long idCarrera, String ciudad);
-    @Query("SELECT new com.tp2.dto.CareerInscriptionsDTO(ce.career.careerName, ce.career.id, COUNT(ce)) FROM CareerStudent ce JOIN Career c ON ce.career.id = c.id GROUP BY ce.id ORDER BY COUNT(ce.career.id)")
+    @Query("SELECT new org.example.dto.CareerInscriptionsDTO(ce.career.careerName, ce.career.id, COUNT(ce)) FROM CareerStudent ce JOIN Career c ON ce.career.id = c.id GROUP BY ce.id ORDER BY COUNT(ce.career.id)")
     List<CareerInscriptionsDTO> getInscriptionSortedByCareer();
-    @Query("SELECT new com.tp2.dto.CareerReportDTO(c.careerName,ce.antiquity,ce.graduated,e.dni,e.LU,e.lastname,e.names) FROM Career c JOIN CareerStudent ce ON c.id = ce.career.id JOIN Student e ON ce.student.id=e.id ORDER BY c.careerName, ce.antiquity")
+    @Query("SELECT new org.example.dto.CareerReportDTO(c.careerName,ce.antiquity,ce.graduated,e.dni,e.LU,e.lastname,e.names) FROM Career c JOIN CareerStudent ce ON c.id = ce.career.id JOIN Student e ON ce.student.id=e.id ORDER BY c.careerName, ce.antiquity")
     List<CareerReportDTO> getReportCareer();
 
 }
